80da627783d25f913c5bf47f628fbfe1ed973942
Shawn H <112985816+HussainBolt@users.noreply.github.com>
2022-11-29 09:27:17 -0800

    populate words

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
         unique: true,
         trim: true,
         lowercase: true,
-        required: true,
+        required: true
     },
     password: {
         type: String,
